c4de0948b262513a63a7ab8764bf655049b52410
Jeff Brown <jeff@jeffandbetsy.net>
2008-12-11 19:49:29 -0600

    add some simple integration tests for contact validation

--- test/integration/ContactTests.groovy
+++ test/integration/ContactTests.groovy
@@ -1,6 +1,32 @@
 class ContactTests extends GroovyTestCase {
 
-    void testSomething() {
+    void testNameTooShort() {
+        def c = new Contact(name:'xx')
+        assertFalse 'validation should have failed', c.validate()
+        
+        def nameError = c.errors.getFieldError('name')
+        assertNotNull 'name error should not have been null', nameError
+        
+        assertTrue 'expected error code was not found', 'Contact.name.size.toosmall' in nameError.codes
+    }
+    
+    void testNameTooLong() {
+        def c = new Contact(name:'123456789012345678901')
+        assertFalse 'validation should have failed', c.validate()
+        
+        def nameError = c.errors.getFieldError('name')
+        assertNotNull 'name error should not have been null', nameError
 
+        assertTrue 'expected error code was not found', 'Contact.name.size.toobig' in nameError.codes
+    }
+    
+    void testNameWithUnderscore() {
+        def c = new Contact(name:'Tony_Iommi')
+        assertFalse 'validation should have failed', c.validate()
+        
+        def nameError = c.errors.getFieldError('name')
+        assertNotNull 'name error should not have been null', nameError
+
+        assertTrue 'expected error code was not found', 'Contact.name.matches.invalid' in nameError.codes
     }
 }
