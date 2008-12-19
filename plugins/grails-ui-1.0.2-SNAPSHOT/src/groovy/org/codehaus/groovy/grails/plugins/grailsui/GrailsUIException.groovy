01b26e3c26e0f8cb61009b7cf9395d44abc3c811
Jeff Brown <jeff@jeffandbetsy.net>
2008-12-19 12:00:42 -0600

    updated plugins

---
+++ plugins/grails-ui-1.0.2-SNAPSHOT/src/groovy/org/codehaus/groovy/grails/plugins/grailsui/GrailsUIException.groovy
@@ -0,0 +1,18 @@
+package org.codehaus.groovy.grails.plugins.grailsui
+class GrailsUIException extends Exception {
+    public GrailsUIException() {
+        super()
+    }
+
+    public GrailsUIException(String message) {
+        super(message)
+    }
+
+    public GrailsUIException(Throwable throwable) {
+        super(throwable)
+    }
+
+    public GrailsUIException(String message, Throwable throwable) {
+        super(message, throwable)
+    }
+}
